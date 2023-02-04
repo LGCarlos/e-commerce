0de6c906769fa8cb365486abbada3e690f016348
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2023-02-04 09:18:42 +0100

    Config EsLint and clean app of unusable logos and code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
