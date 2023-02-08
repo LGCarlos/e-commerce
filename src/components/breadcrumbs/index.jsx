7dbc91c6a4960ae423a6c81d9257fd2cef204f03
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2023-02-08 20:59:21 +0100

    Styling Header component

--- src/components/breadcrumbs/index.jsx
+++ src/components/breadcrumbs/index.jsx
@@ -2,18 +2,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { CRUMBS } from '../../constants';
+import style from './breadcrumbs.module.css';
 
 function Breadcrumbs() {
   return (
     <>
-      <div className="breadcrumbs">
+      <div className={style.breadcrumbs}>
         {
             CRUMBS
               ? (
                 CRUMBS.map((e) => (
                   <div key={e.name} id={e.name}>
                     <Link to={e.path}>
-                      <p>{e.name}</p>
+                      <p>
+                        {e.name}
+                        {' '}
+                        /
+                      </p>
+
                     </Link>
                   </div>
                 ))
