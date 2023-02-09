819d3a69ace2a44233e1d1e771162a0f9a9206e9
Carlos Lopez <lopezgonzalezcarlos17@gmail.com>
2023-02-09 16:34:27 +0100

    Styling app component

--- src/context/StaticContext.jsx
+++ src/context/StaticContext.jsx
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
 
 export const ContextProducts = createContext();
+export const ContextSession = createContext();
 export const ContextBasket = createContext(0);
